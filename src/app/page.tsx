8c0de74a7f3b4fb15449fa4d3f87a1e489b76c0e
Fernando Filho <fernando@dimap.ufrn.br>
2023-06-20 11:07:41 -0300

    update cards in dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,74 @@
 import {
   Card,
   Grid,
+  Metric,
   Title,
+  Flex,
+  Icon,
   Text,
   Tab,
   TabList,
   TabGroup,
   TabPanel,
   TabPanels,
+  Color,
+  AreaChart,
 } from "@tremor/react";
 
+// import heroicons react components
+import { TicketIcon, UserGroupIcon, TrashIcon } from "@heroicons/react/24/solid";
+
+const categories: {
+  title: string;
+  metric: string;
+  icon: any;
+  color: Color;
+}[] = [
+  {
+    title: "Receita",
+    metric: "$ 23,456,456",
+    icon: TicketIcon,
+    color: "indigo",
+  },
+  {
+    title: "Despesa",
+    metric: "$ 13,123",
+    icon: TrashIcon,
+    color: "fuchsia",
+  },
+  {
+    title: "Tributos",
+    metric: "456",
+    icon: UserGroupIcon,
+    color: "amber",
+  },
+];
+
+const data = [
+  {
+    Month: "Jan 21",
+    Sales: 2890,
+    Profit: 2400,
+  },
+  {
+    Month: "Feb 21",
+    Sales: 1890,
+    Profit: 1398,
+  },
+  // ...
+  {
+    Month: "Jan 22",
+    Sales: 3890,
+    Profit: 2980,
+  },
+];
+
 export default function DashboardExample() {
+  // 
   return (
     <main className="px-12 py-12">
+      <Title>Painel</Title>
+      <Text>Acompanhe a evolução da receita, despesa e tributos.</Text>
       <TabGroup className="mt-6">
         <TabList>
           <Tab>Overview</Tab>
@@ -22,24 +78,36 @@ export default function DashboardExample() {
         </TabList>
         <TabPanels>
           <TabPanel>
-            <Grid numItemsMd={2} numItemsLg={3} className="gap-6 mt-6">
-              <Card>
-                {/* Placeholder to set height */}
-                <div className="h-28" />
+          <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
+            {categories.map((item) => (
+              <Card key={item.title} decoration="top" decorationColor={item.color}>
+                <Flex justifyContent="start" className="space-x-4">
+                  <Icon icon={item.icon} variant="light" size="xl" color={item.color} />
+                  <div className="truncate">
+                    <Text>{item.title}</Text>
+                    <Metric className="truncate">{item.metric}</Metric>
+                  </div>
+                </Flex>
               </Card>
-              <Card>
-                {/* Placeholder to set height */}
-                <div className="h-28" />
-              </Card>
-              <Card>
-                {/* Placeholder to set height */}
-                <div className="h-28" />
-              </Card>
-            </Grid>
+            ))}
+          </Grid>
             <div className="mt-6">
               <Card>
-                <div className="h-80" />
+                <Title>Performance</Title>
+                <Text>Comparison between Sales and Profit</Text>
+                <AreaChart
+                  className="mt-4 h-80"
+                  data={data}
+                  categories={["Sales", "Profit"]}
+                  index="Month"
+                  colors={["indigo", "fuchsia"]}
+                  yAxisWidth={60}
+                  valueFormatter={(number: number) =>
+                    `$ ${Intl.NumberFormat("us").format(number).toString()}`
+                  }
+                />
               </Card>
+
             </div>
           </TabPanel>
           <TabPanel>
